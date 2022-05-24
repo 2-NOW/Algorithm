e98c0864bc615067194997b65268cb2cc4e9ae14
이현재 <71202076+2-NOW@users.noreply.github.com>
2022-05-24 20:57:58 +0900

    Time: 94 ms (66.06%), Space: 45.5 MB (80.61%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.js
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.js
@@ -3,11 +3,5 @@
  * @return {number[]}
  */
 var buildArray = function(nums) {
-  const ans = [];
-  
-  for(let i = 0; i < nums.length; i++) {
-    ans.push(nums[nums[i]])
-  }
-  
-  return ans;
+  return nums.map((_, i) => nums[nums[i]]);
 };
