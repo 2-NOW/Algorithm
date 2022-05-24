15b34cbf445868431bc2a57c63be5d3955c44246
이현재 <71202076+2-NOW@users.noreply.github.com>
2022-05-24 14:25:52 +0900

    Time: 116 ms, Memory: 42 MB - LeetHub

---
+++ merge-sorted-array/merge-sorted-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+  nums1.splice(m, nums1.length - m);
+  nums2.splice(n, nums2.length - n);
+  
+  for(let i of nums2) {
+    nums1.push(i)
+  }
+  
+  nums1.sort((a, b) => a - b);
+};
