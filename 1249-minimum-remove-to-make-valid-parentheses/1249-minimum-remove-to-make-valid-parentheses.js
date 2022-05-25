83fc06a3b888f35c8562fa60857bb4071afcfb5c
이현재 <71202076+2-NOW@users.noreply.github.com>
2022-05-25 22:46:34 +0900

    Time: 2312 ms (5.05%), Space: 81.8 MB (5.05%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.js
@@ -0,0 +1,43 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var minRemoveToMakeValid = function(s) {
+  let open = 0;
+  
+  // s에 ( 가 있으면 더 지나가면 )가 있어야함.
+  // minimum으로
+  // 먼저 닫힌 괄호 있으면 바로 없앰
+  // 열린괄호면 진행 -> 쭉 열린괄호가 있어야됨
+  // 쭉 열리다가 닫히면 그때부턴 닫힌괄호가 나와야됨
+  // 열린괄호 수만큼 닫힌괄호가 생기면 그 후로는 괄호가 없어야됨
+  
+  for (let i = 0; i < s.length; i++) {
+    if(s.charAt(i) === '(') open++;
+    else if(s.charAt(i) === ')') open--
+    
+    if(open < 0) {
+      s = s.substring(0, i) + s.substring(i + 1);
+      open++;
+      i--;
+    }
+  }
+  
+  let close = 0;
+  // 괄호가 열려있는채로 끝났다 ?
+  // 그럼 뒤에서 한번 더 계산
+  console.log(open)
+  if(open > 0) {
+    for (let j = s.length - 1; j >= 0; j--) {
+      if(s.charAt(j) === '(') close--;
+      else if(s.charAt(j) === ')') close++;
+      console.log(close, j)
+      if(close < 0) {
+        s = s.substring(0, j) + s.substring(j + 1)
+        close++;
+      }
+    }
+  }
+
+  return s;
+};
