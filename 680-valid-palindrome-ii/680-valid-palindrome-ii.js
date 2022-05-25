c3300dd0a566a1fb38ded0637cc80d4784328bb0
이현재 <71202076+2-NOW@users.noreply.github.com>
2022-05-25 09:36:13 +0900

    Time: 2171 ms (5.04%), Space: 55.6 MB (11.52%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.js
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.js
@@ -8,23 +8,19 @@ var validPalindrome = function(s) {
   // 앞에서 건너뛴거, 뒤에서 건너뛴거 둘 다 계산해서
   // 두개 중 하나라도 true면 true
   // 둘 다 false가 나오면 false
-  let l = 0;
   let r = s.length - 1;
+  
   const check = (left, right) => {
-    console.log(left, right)
-    while(left < right) {
-      console.log(s[left], s[right])
-      if(s[left] !== s[right]) return false;
-      left++;
+    for (let i = left; i < s.length/2; i++) {
+      console.log(s[i], s[right])
+      if(s[i] !== s[right]) return false;
       right--;
     }
     return true;
   }
-  console.log(l,r)
-  while (l < r) {
-    console.log(l, r)
-    if(s[l] !== s[r]) return check(l, r-1) || check(l+1, r)
-    l++;
+
+  for (let i = 0; i < s.length/2; i++) {
+    if(s[i] !== s[r]) return check(i, r-1) || check(i+1, r)
     r--;
   }
   
