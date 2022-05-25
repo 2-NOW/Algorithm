c296aab85bf10bbf8eabf9bc02b9327b1340153f
이현재 <71202076+2-NOW@users.noreply.github.com>
2022-05-25 17:48:28 +0900

    Time: 94 ms (32.68%), Space: 41.7 MB (94.53%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+  const brackets = {'(':')', '{':'}', '[':']'};
+  const open = ['(', '{', '['];
+  const stack = [];
+    
+  for (let i = 0; i < s.length; i++) {
+    if(open.includes(s.charAt(i))) {
+      stack.push(brackets[s.charAt(i)])
+    } else if (s.charAt(i) !== stack.pop()) return false;
+  }
+  console.log(stack)
+  return !!!stack.length ? true : false;
+};
