93795f46dd1730811179fe8b4c051cafde370013
이현재 <71202076+2-NOW@users.noreply.github.com>
2022-05-29 10:58:14 +0900

    Time: 71 ms (70.67%), Space: 41.7 MB (80.00%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.js
+++ 991-broken-calculator/991-broken-calculator.js
@@ -12,5 +12,8 @@ var brokenCalc = function(startValue, target) {
     count++;
   }
   
+  // 나누기 2를 했을 때 target이 startValue 보다 작아지면
+  // 그만큼은 어차피 더하기 step을 밟아야 하므로
+  // startValue - target >= 0 의 값이 나온다.
   return count += startValue - target;
 };
