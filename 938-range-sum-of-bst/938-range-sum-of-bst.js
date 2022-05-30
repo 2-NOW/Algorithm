356d7dc9b4578d36fee4f26111daa4dcf7af1486
이현재 <71202076+2-NOW@users.noreply.github.com>
2022-05-30 23:18:58 +0900

    Time: 264 ms (25.97%), Space: 97.1 MB (10.61%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.js
+++ 938-range-sum-of-bst/938-range-sum-of-bst.js
@@ -13,20 +13,47 @@
  * @return {number}
  */
 var rangeSumBST = function(root, low, high) {
-  const visited = [];
-  const stack = [root];
   let answer = 0;
   
-  while(stack.length !== 0) {
-    const node = stack.pop();
+  // 재귀풀이
+  // 한 단위로 나눠서 생각하기
+  // 노드 도착하면 해야하는것
+  // value 확인
+  // left 있으면 left 들어가기
+  // right 있으면 right 들어가기
+  // 둘 다 없으면 return
+  const recursive = (node, low, high) => {
     if(high >= node.val && node.val >= low) answer += node.val;
 
-    if(!visited.includes(node.val)) {
-      if(node.right) stack.push(node.right);
-      if(node.left) stack.push(node.left);
-      visited.push(node.val);
-    }
+    if(node.left) recursive(node.left, low, high);
+    if(node.right) recursive(node.right, low, high);
+    return;
   }
   
+  recursive(root, low, high);
+  
   return answer;
+  
+  
+  
+  
+  
+  
+  // 스택풀이
+//   const visited = [];
+//   const stack = [root];
+//   let answer = 0;
+  
+//   while(stack.length !== 0) {
+//     const node = stack.pop();
+//     if(high >= node.val && node.val >= low) answer += node.val;
+
+//     if(!visited.includes(node.val)) {
+//       if(node.right) stack.push(node.right);
+//       if(node.left) stack.push(node.left);
+//       visited.push(node.val);
+//     }
+//   }
+    
+//   return answer;
 };
