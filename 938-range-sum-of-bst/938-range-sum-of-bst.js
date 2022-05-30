9237227b2f545fb95e6cf5e2a6d91bfd7ea1b943
이현재 <71202076+2-NOW@users.noreply.github.com>
2022-05-30 23:30:22 +0900

    Time: 229 ms (63.02%), Space: 96.8 MB (19.62%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.js
+++ 938-range-sum-of-bst/938-range-sum-of-bst.js
@@ -12,32 +12,36 @@
  * @param {number} high
  * @return {number}
  */
-var rangeSumBST = function(root, low, high) {
-  let answer = 0;
+var rangeSumBST = function(root, low, high, sum = 0) {
+  // 함수 자체를 재귀로
+  // 없는 노드가 들어왔을때 (right 혹은 left가 없을 때) 바로 sum을 return 해줌 (기본값 0)
+  if(!root) return sum;
   
-  // 재귀풀이
-  // 한 단위로 나눠서 생각하기
-  // 노드 도착하면 해야하는것
-  // value 확인
-  // left 있으면 left 들어가기
-  // right 있으면 right 들어가기
-  // 둘 다 없으면 return
-  const recursive = (node, low, high) => {
-    if(high >= node.val && node.val >= low) answer += node.val;
+  if(high >= root.val && root.val >= low) sum += root.val;
+  
+  // leaf node에서는 root.left, root.right의 value가 없으므로 0값으로 return 됨.
+  return sum + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+  
+//   let answer = 0;
+  
+//   // 재귀풀이
+//   // 한 단위로 나눠서 생각하기
+//   // 노드 도착하면 해야하는것
+//   // value 확인
+//   // left 있으면 left 들어가기
+//   // right 있으면 right 들어가기
+//   // 둘 다 없으면 return
+//   const recursive = (node, low, high) => {
+//     if(high >= node.val && node.val >= low) answer += node.val;
 
-    if(node.left) recursive(node.left, low, high);
-    if(node.right) recursive(node.right, low, high);
-    return;
-  }
+//     if(node.left) recursive(node.left, low, high);
+//     if(node.right) recursive(node.right, low, high);
+//     return;
+//   }
   
-  recursive(root, low, high);
+//   recursive(root, low, high);
   
-  return answer;
-  
-  
-  
-  
-  
+//   return answer;
   
   // 스택풀이
 //   const visited = [];
