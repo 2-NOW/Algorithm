c21b7e39563c237072e68101319a6b5b9298708c
이현재 <71202076+2-NOW@users.noreply.github.com>
2022-05-31 14:05:00 +0900

    Time: 82 ms (91.94%), Space: 45 MB (64.53%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -14,28 +14,41 @@ var numIslands = function(grid) {
   
   function recursive(grid, row, col) {
     grid[row][col] = '0';
+    
     if(row + 1 < grid.length && grid[row + 1][col] === '1') recursive(grid, row + 1, col);
     if(col + 1 < grid[0].length && grid[row][col + 1] === '1') recursive(grid, row, col + 1);
     if(row - 1 >= 0 && grid[row - 1][col] === '1') recursive(grid, row - 1, col);
     if(col - 1 >= 0 && grid[row][col - 1] === '1') recursive(grid, row, col - 1);
-    }
+  }
   
-  while(stack.length !== 0) {
-    const cur = stack.pop();
-    if(grid[cur[0]][cur[1]] === "1") {
-      recursive(grid, cur[0], cur[1])
-      count += 1;
+  for(let row = 0; row < grid.length; row++) {
+    for(let col = 0; col < grid[0].length; col++) {
+      if(grid[row][col] === '1') {
+        recursive(grid, row, col);
+        count += 1;
+      }
     }
-    if(curRow < grid.length - 1) curRow += 1
-    else if(curCol < grid[0].length - 1) {
-      curRow = 0;
-      curCol += 1;
-    }
-    
-    if(cur[0] !== curRow || cur[1] !== curCol) stack.push([curRow, curCol]);
   }
   
   return count;
+  // while문은 stack에 넣었다 뺐다 하는 시간이 들어가서
+  // 이중for문이 더 나은 런타임을 갖는다.
+//   while(stack.length !== 0) {
+//     const cur = stack.pop();
+//     if(grid[cur[0]][cur[1]] === "1") {
+//       recursive(grid, cur[0], cur[1])
+//       count += 1;
+//     }
+//     if(curRow < grid.length - 1) curRow += 1
+//     else if(curCol < grid[0].length - 1) {
+//       curRow = 0;
+//       curCol += 1;
+//     }
+    
+//     if(cur[0] !== curRow || cur[1] !== curCol) stack.push([curRow, curCol]);
+//   }
+  
+//   return count;
   
     // 재귀로하면 스택에 너무 많이 쌓일 것 같다.
   // function recursive0(grid, a, b) {
