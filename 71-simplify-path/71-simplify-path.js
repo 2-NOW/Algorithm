204c8a0000f7beebff19adaa7b43de79923b5e7f
이현재 <71202076+2-NOW@users.noreply.github.com>
2022-06-12 21:17:36 +0900

    Time: 137 ms (8.52%), Space: 44.1 MB (72.10%) - LeetHub

--- 71-simplify-path/71-simplify-path.js
+++ 71-simplify-path/71-simplify-path.js
@@ -8,7 +8,6 @@ var simplifyPath = function(path) {
     // 절대경로
     // 경로를 만나면 -> 경로를 answer에 넣음
     // .. 이면 answer에서 하나 뺌 pop
-    // 뺄게 없으면 안뺌
     for(let i=0; i<arr.length; i++) {
       if(arr[i] !== '' && arr[i] !== '.') {
         if(arr[i] === '..') paths.pop();
@@ -17,22 +16,4 @@ var simplifyPath = function(path) {
     }
 
     return '/' + paths.join('/');
-  
-  
-//     const split = path.split('/');
-//     const finalPath = [];
-//     while(split.length) {
-//     const target = split.shift();
-    
-        // if (target === '' || target === '.') {
-        //   continue;
-        // } else if (target === '..') {
-        //   finalPath.pop();
-        // } else {
-        //   finalPath.push(target);
-        // }
-//     }
-  
-//   return '/' + finalPath.join('/');
-
 };
