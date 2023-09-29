f28ad064a3bbffc11361a157f5dca3958b736a6d
이현재 <71202076+2-NOW@users.noreply.github.com>
2023-09-29 12:58:07 +0900

    Time: 0 ms (100%), Space: 2.2 MB (17.91%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.rs
+++ 0014-longest-common-prefix/0014-longest-common-prefix.rs
@@ -3,13 +3,30 @@ impl Solution {
         if strs.is_empty() {
             return "".into();
         }
-        let prefix = &strs[0]; 
-        for i in 0..prefix.len() {
-            let c = &prefix[i..=i];
-            if strs.iter().any(|s| s.len() <= i || &s[i..=i] != c) {
-                return prefix[..i].into();
+
+        let min_length = strs.iter().map(|s| s.len()).min().unwrap();
+        let mut low = 1;
+        let mut high = min_length;
+
+        while low <= high {
+            let mid = (low + high) / 2;
+            if Solution::is_common_prefix(&strs, mid) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
-        prefix.into()
+
+        strs[0][..(low + high) / 2].to_string()
+    }
+
+    fn is_common_prefix(strs: &Vec<String>, len: usize) -> bool {
+        let str1 = &strs[0][..len];
+        for i in 1..strs.len() {
+            if !strs[i].starts_with(str1) {
+                return false;
+            }
+        }
+        true
     }
 }
