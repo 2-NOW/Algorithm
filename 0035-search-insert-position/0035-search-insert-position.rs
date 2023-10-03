1baaf1fcb4fdbe9d968066111161331d468acff7
이현재 <71202076+2-NOW@users.noreply.github.com>
2023-10-03 23:15:47 +0900

    Time: 1 ms (84.47%), Space: 2.2 MB (17.72%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.rs
+++ 0035-search-insert-position/0035-search-insert-position.rs
@@ -1,19 +1,28 @@
 impl Solution {
     pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-        let mut l = 0;
-        let mut r = nums.len() as i32 - 1;
-        
-        while l <= r {
-            let c = l + (r - l) / 2;
-            if nums[c as usize] == target {
-                return c;
-            } else if nums[c as usize] < target {
-                l = c + 1;
-            } else {
-                r = c - 1;
-            }
+        match nums.binary_search(&target) {
+            Ok(index) => index as i32,
+            Err(index) => index as i32,
         }
-
-        return l;
     }
 }
+
+// impl Solution {
+//     pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+//         let mut l = 0;
+//         let mut r = nums.len() as i32 - 1;
+        
+//         while l <= r {
+//             let c = l + (r - l) / 2;
+//             if nums[c as usize] == target {
+//                 return c;
+//             } else if nums[c as usize] < target {
+//                 l = c + 1;
+//             } else {
+//                 r = c - 1;
+//             }
+//         }
+
+//         return l;
+//     }
+// }
