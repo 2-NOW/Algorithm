8039d3a271f31353c49ba8d9974033d9969dfc1a
이현재 <71202076+2-NOW@users.noreply.github.com>
2023-10-04 23:52:16 +0900

    Time: 0 ms (100%), Space: 2.2 MB (43.6%) - LeetHub

--- 0067-add-binary/0067-add-binary.rs
+++ 0067-add-binary/0067-add-binary.rs
@@ -1,35 +1,42 @@
 impl Solution {
     pub fn add_binary(a: String, b: String) -> String {
-    let (lena, lenb) = (a.len(), b.len());
-        let maxlen = lena.max(lenb);
-        let mut ans = String::new();
+    let (a_len, b_len) = (a.len(), b.len());
+        let max_len = a_len.max(b_len);
+        let mut result = String::new();
         let mut carry = false;
         
-        // Pad the shorter string with '0's
-        let a = format!("{:0>width$}", a, width=maxlen);
-        let b = format!("{:0>width$}", b, width=maxlen);
+        // 왼쪽에 0 채워넣기
+        let a = format!("{:0>width$}", a, width=max_len);
+        let b = format!("{:0>width$}", b, width=max_len);
 
-        for i in (0..maxlen).rev() {
+        // 최대 길이의 마지막 index 부터
+        for i in (0..max_len).rev() {
             let (c1, c2) = (a.as_bytes()[i] as char, b.as_bytes()[i] as char);
+
             match (c1, c2, carry) {
                 ('1', '1', false) | ('1', '0', true) | ('0', '1', true) => {
-                    ans.push('0');
+                    result.push('0');
                     carry = true;
                 }
                 ('1', '0', false) | ('0', '1', false) | ('0', '0', true) => {
-                    ans.push('1');
+                    result.push('1');
                     carry = false;
                 }
                 ('0', '0', false) => {
-                    ans.push('0');
+                    result.push('0');
                 }
                 ('1', '1', true) => {
-                    ans.push('1');
+                    result.push('1');
                 }
                 _ => unreachable!(),
             }
         }
-        if carry == true { ans.push('1'); }
-        ans.chars().rev().collect()
+
+        // 마지막 carry 계산
+        if carry == true { 
+            result.push('1'); 
+        }
+
+        result.chars().rev().collect()
     }
 }
