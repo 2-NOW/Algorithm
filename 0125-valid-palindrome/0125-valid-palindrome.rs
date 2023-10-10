50925cae9b631914a6b69079b35af1b55eb5fb69
이현재 <71202076+2-NOW@users.noreply.github.com>
2023-10-10 16:46:20 +0900

    Time: 0 ms (100%), Space: 2.6 MB (27.16%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.rs
+++ 0125-valid-palindrome/0125-valid-palindrome.rs
@@ -1,8 +1,35 @@
 impl Solution {
     pub fn is_palindrome(s: String) -> bool {
-        let _s = s.chars()
+        if s.is_empty() { return true; }
+
+        let _s: Vec<char> = s.chars()
                     .filter(|c| c.is_ascii_alphanumeric())
-                    .map(|c| c.to_ascii_lowercase());
-        _s.clone().eq(_s.rev())
+                    .map(|c| c.to_ascii_lowercase())
+                    .collect();
+
+        if _s.is_empty() { return true; }
+
+        let mut start = 0;
+        let mut end = _s.len()-1;
+
+        while start < end {
+            let (l, r) = (_s[start], _s[end]);
+            if l == r {
+                start += 1;
+                end -= 1;
+            } else {
+                return false;
+            }
+        }
+        true
     }
 }
+
+// impl Solution {
+//     pub fn is_palindrome(s: String) -> bool {
+//         let _s = s.chars()
+//                     .filter(|c| c.is_ascii_alphanumeric())
+//                     .map(|c| c.to_ascii_lowercase());
+//         _s.clone().eq(_s.rev())
+//     }
+// }
