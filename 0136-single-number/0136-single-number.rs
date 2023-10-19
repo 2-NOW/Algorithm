e0b08a45e8123f925e7d26bee7a985c4b4a7194a
이현재 (whale) <71202076+2-NOW@users.noreply.github.com>
2023-10-19 22:56:52 +0900

    Time: 2 ms (84.25%), Space: 2.1 MB (87.02%) - LeetHub

--- 0136-single-number/0136-single-number.rs
+++ 0136-single-number/0136-single-number.rs
@@ -1,11 +1,5 @@
 impl Solution {
     pub fn single_number(nums: Vec<i32>) -> i32 {
-        let mut xor_result = 0;
-
-        for num in nums {
-            xor_result ^= num;
-        }
-
-        xor_result
+        nums.into_iter().fold(0, |acc, cur| acc ^ cur)
     }
 }
