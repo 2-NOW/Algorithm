113ecb4881e74b0cd5f5e12f73eab3919e15fe0a
이현재 (whale) <71202076+2-NOW@users.noreply.github.com>
2023-10-19 22:33:58 +0900

    Time: 2 ms (84.25%), Space: 2.2 MB (58.56%) - LeetHub

--- 0136-single-number/0136-single-number.rs
+++ 0136-single-number/0136-single-number.rs
@@ -2,16 +2,12 @@ use std::collections::HashSet;
 
 impl Solution {
     pub fn single_number(nums: Vec<i32>) -> i32 {
-        let mut set = HashSet::new();
+        let mut xor_result = 0;
 
         for num in nums {
-            if !set.contains(&num) {
-                set.insert(num);
-            } else {
-                set.remove(&num);
-            }
+            xor_result ^= num;
         }
 
-        *set.iter().next().unwrap_or(&0)
+        xor_result
     }
 }
