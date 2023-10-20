f996a62df07a2debf8a75dc01a6e5ad30016cd95
이현재 (whale) <71202076+2-NOW@users.noreply.github.com>
2023-10-21 00:05:05 +0900

    Time: 0 ms (100%), Space: 2 MB (51.69%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.rs
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.rs
@@ -3,9 +3,13 @@ impl Solution {
         let (mut result, mut number) = (String::new(), column_number);
 
         while number > 0 {
+            // A의 숫자 1 뺴기
             number -= 1;
+            // ASCII로 변환 (+ A)
             let char_code = ((number % 26) as u8) + b'A';
+            // 문자열 앞에 추가하기
             result.insert(0, char_code as char);
+            // 다음 자리수로 넘어가기
             number /= 26;
         }
 
